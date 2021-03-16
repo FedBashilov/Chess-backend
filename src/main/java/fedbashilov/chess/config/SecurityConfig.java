c2a1a45dae6bc4d997e94f498147520fba3f640b
FedBashilov <>
2021-03-16 22:30:51 +0300

    Fix CORS

---
+++ src/main/java/fedbashilov/chess/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package fedbashilov.chess.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
--- src/main/java/fedbashilov/chess/config/WebSocketConfig.java
+++ src/main/java/fedbashilov/chess/config/WebSocketConfig.java
@@ -1,23 +1,26 @@
 package fedbashilov.chess.config;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket")
-                .setAllowedOrigins("*")
-                .withSockJS();
-    }
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app")
                 .enableSimpleBroker("/message");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        StompWebSocketEndpointRegistration registration = registry.addEndpoint("/socket");
+        registration.setAllowedOriginPatterns("*");
+        registration.withSockJS();
+    }
+
+
 }
